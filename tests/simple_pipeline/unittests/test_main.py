dff9633e35a6a1eb7c951d5f5c86d39b6ccbb1c2
Pablo Pardo Garcia <pablo@glassflow.dev>
2025-01-16 13:28:38 +0100

    delete tests

--- tests/simple_pipeline/unittests/test_main.py
+++
@@ -1,9 +0,0 @@
-from pipelines.simple_pipeline.handler import handler
-import logging
-
-
-def test_handler_new_field():
-    log = logging.getLogger()
-    data = handler(data={}, log=log)
-
-    assert data == {"new_field": "new_value"}
